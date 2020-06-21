9cd0b4ce444370a3b426c99d56aac05c784f62d5
takayuki nakamura <ktkr7195@gmai.com>
2020-06-21 13:34:09 +0900

    Fix:edit header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 module ApplicationHelper
-
   def full_title(page_title = '')
-    base_title = "Fodmap"
+    base_title = 'Fodmap'
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      page_title + ' | ' + base_title
     end
   end
-
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
+RSpec.describe 'StaticPages', type: :request do
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
-  describe "GET /" do
-    it "should get root" do
+  describe 'GET /' do
+    it 'should get root' do
       get '/'
       expect(response).to have_http_status(200)
-      expect(response.body).to match(/<title>home | #{base_title}<\/title>/i)
+      expect(response.body).to match(%r{<title>home | #{base_title}</title>}i)
     end
   end
 end
