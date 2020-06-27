fa25b47ce786afad598c4fe0e0ac463fee02dc7c
takayuki nakamura <ktkr7195@gmai.com>
2020-06-27 20:53:04 +0900

    Add recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
   devise_for :users
   root to:'homes#index'
+  get '/recipe', to:'recipe#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
