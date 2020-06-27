c2ca736bb2587b5f8493c9c87f528e749bc46cc4
takayuki nakamura <ktkr7195@gmai.com>
2020-06-27 16:17:18 +0900

    Add sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,19 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # default url
+  config.action_mailer.default_url_options = {  host: 'localhost' }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Rails.application.credentials.gmail[:user_name],
+    :password => Rails.application.credentials.gmail[:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
@@ -57,5 +68,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
